a456bb3b2461f6066f800a342ebc97cd76ffca3a
Marina <gtellier.mecanics@gmail.com>
2022-07-27 16:49:07 +0300

    Числа кратные 3

---
+++ task1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Задайте значение M");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте значение N");
+int numberTwo = Convert.ToInt32(Console.ReadLine());
+Recursion(numberOne, numberTwo);
+
+void Recursion(int numberOne, int numberTwo)
+{
+    if (numberOne > numberTwo) return;
+    if (numberOne%3 ==0)
+    {
+    Console.Write(numberOne + "  ");
+    }
+    numberOne++;
+    Recursion(numberOne, numberTwo);
+}
