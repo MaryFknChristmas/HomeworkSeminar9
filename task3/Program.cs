eb14b5e2d7ea44c9a8ae905dba902f82d492ce21
Marina <gtellier.mecanics@gmail.com>
2022-07-27 16:50:05 +0300

    Функция Аккермана

---
+++ task3/Program.cs
@@ -0,0 +1,11 @@
+﻿int m = 3;
+int n = 2;
+Console.WriteLine(Akkerman(m, n));
+
+int Akkerman(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (m > 0 && n == 0) return Akkerman(m - 1, 1);
+    else if (m > 0 && n > 0) return Akkerman(m - 1, Akkerman(m, n - 1));
+    return Akkerman(m, n);
+}
