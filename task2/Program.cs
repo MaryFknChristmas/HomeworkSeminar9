48e3709fe41c3d4a71c6e68dc3214cd8fce5f004
Marina <gtellier.mecanics@gmail.com>
2022-07-27 16:49:39 +0300

    Сумма чисел

---
+++ task2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Задайте значение M");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте значение N");
+int numberTwo = Convert.ToInt32(Console.ReadLine());
+Recursion(numberOne, numberTwo);
+
+void Recursion(int numberOne, int numberTwo, int sum = 0)
+{
+    if (numberOne > numberTwo)
+    {
+        Console.WriteLine(sum);
+        return;
+    }
+    sum += numberOne;
+    numberOne++;
+    Recursion(numberOne, numberTwo, sum);
+}
